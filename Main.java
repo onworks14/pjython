f0090513d0681d4543b4c760a120abe87db8b76f
onworks14 <onworks14@gmail.com>
2023-12-09 14:40:22 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,7 @@
+public abstract class Main {
+  public static void foo(int x) {}
+  public static void foo(float x) {}
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
